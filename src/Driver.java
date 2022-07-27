4731a45f645eb079ea0cb8697f7ac1f082413aee
AqibChattha <aqibali543@gmail.com>
2022-07-27 15:13:37 -0700

    create - java project
    The java runtime environment used in this project is JavaSE-17. The IDE used is Eclipse.

---
+++ src/Driver.java
@@ -0,0 +1,18 @@
+/**
+ * Driver class is the main class, it contain the main function.
+ * 
+ * @author Aqib Chattha
+ * @version 0.1, 2022-27-07
+ * @since JDK17.0.4
+ */
+public class Driver {
+
+	/**
+	 * Main method for the game application.
+	 * @param args an array of arguments
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
